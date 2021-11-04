a128069221afb2ab243c1a3cbafc29ade8b1b85a
Mari Ohbuchi <ohbuchim@amazon.co.jp>
2021-11-04 19:49:12 +0900

    test

--- setenv.sh
+++ setenv.sh
@@ -1,5 +1,13 @@
+ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+# preprocess
+INPUT_DATA_PATH=$(python setenv.py preprocess input-data-path)
+OUTPUT_DATA_PATH=$(python setenv.py preprocess input-data-path)
 # train
-TRAIN_URL=$(python setenv.py train train_url)
-DATA_PATH=$(python setenv.py train data_path)
-BATCH_SIZE=$(python setenv.py train hyper_parameter batch_size)
-EPOCH=$(python setenv.py train hyper_parameter epoch)
+TRAIN_JOB_NAME=$(python setenv.py train train-job-name)
+TRAIN_IMAGE_URL=$(python setenv.py train train-image-url)
+TRAIN_DATA_PATH=$(python setenv.py train data-path)
+BATCH_SIZE=$(python setenv.py train hyper-parameters batch-size)
+EPOCH=$(python setenv.py train hyper-parameters epoch)
+# evaluate
+MODEL_PATH=$(python setenv.py evaluate model-path)
+EVALUATE_RESULT=$(python setenv.py evaluate evaluate-result)
