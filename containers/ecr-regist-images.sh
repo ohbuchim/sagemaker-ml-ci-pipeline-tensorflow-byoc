bf98f19f77d97bc17715895756263cbf9555ab8c
Mari Ohbuchi <ohbuchim@amazon.co.jp>
2021-11-08 18:39:49 +0900

    test

--- containers/ecr-regist-images.sh
+++ containers/ecr-regist-images.sh
@@ -17,8 +17,8 @@ aws ecr get-login-password | docker login --username AWS --password-stdin $REGIS
 aws ecr create-repository --repository-name $ECR_REPOGITORY
 
 docker build -t $ECR_REPOGITORY containers/train/
-docker tag "${ECR_REPOGITORY}:${IMAGE_TAG}" $IMAGE_URI
-docker push $IMAGE_URI
+docker tag ${ECR_REPOGITORY} $IMAGE_URI:${IMAGE_TAG}
+docker push $IMAGE_URI:${IMAGE_TAG}
 
 echo "Container registered. URI:${IMAGE_URI}"
 
