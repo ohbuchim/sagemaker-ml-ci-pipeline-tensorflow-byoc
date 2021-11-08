3fe07e0d332039dfae3017401aca6f67efcbc035
Mari Ohbuchi <ohbuchim@amazon.co.jp>
2021-11-08 09:13:52 +0900

    test

--- containers/ecr-regist-train.sh
+++ containers/ecr-regist-train.sh
@@ -3,9 +3,26 @@
 # REGION='us-east-1'
 REGION=${AWS_DEFAULT_REGION}
 REGISTRY_URL="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com" 
-ECR_REPOGITORY='sagemaker-tf-nightly-gpu'
+# ECR_REPOGITORY='sagemaker-tf-nightly-gpu'
 IMAGE_TAG=$(git rev-parse --short HEAD)
-IMAGE_URI="${REGISTRY_URL}/${ECR_REPOGITORY}"
+
+echo "========"
+echo ${IMAGE_TAG}
+
+# train
+IMAGE_URI="${REGISTRY_URL}/${ECR_REPOGITORY}-train"
+
+aws ecr get-login-password | docker login --username AWS --password-stdin $REGISTRY_URL
+aws ecr create-repository --repository-name $ECR_REPOGITORY
+
+docker build -t $ECR_REPOGITORY containers/train/
+docker tag "${ECR_REPOGITORY}${IMAGE_TAG}" $IMAGE_URI
+docker push $IMAGE_URI
+
+echo "Container registered. URI:${IMAGE_URI}"
+
+# evaluate
+IMAGE_URI="${REGISTRY_URL}/${ECR_REPOGITORY}-evaluate"
 
 aws ecr get-login-password | docker login --username AWS --password-stdin $REGISTRY_URL
 aws ecr create-repository --repository-name $ECR_REPOGITORY
--- setenv.sh
+++ setenv.sh
@@ -1,2 +1,3 @@
 ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
+ECR_REPOGITORY_PREFIX=$(python setenv.py config image-name-prefix)
 # REGION=$(python setenv.py config region)
