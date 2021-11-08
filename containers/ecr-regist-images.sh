25bf499f5e910f5897c1e611d55d7864312a82e5
Mari Ohbuchi <ohbuchim@amazon.co.jp>
2021-11-08 12:47:00 +0900

    test

--- containers/ecr-regist-images.sh
+++ containers/ecr-regist-images.sh
@@ -6,7 +6,6 @@ REGISTRY_URL="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
 # ECR_REPOGITORY='sagemaker-tf-nightly-gpu'
 IMAGE_TAG=":$(git rev-parse --short HEAD)"
 
-echo ${ECR_REPOGITORY_PREFIX}
 echo "========"
 echo ${IMAGE_TAG}
 
@@ -14,14 +13,12 @@ echo ${IMAGE_TAG}
 ECR_REPOGITORY="${ECR_REPOGITORY_PREFIX}-train"
 IMAGE_URI="${REGISTRY_URL}/${ECR_REPOGITORY}"
 
-echo "repo name prefix=-=-===--="
-echo ${ECR_REPOGITORY_PREFIX}
-
 aws ecr get-login-password | docker login --username AWS --password-stdin $REGISTRY_URL
 aws ecr create-repository --repository-name $ECR_REPOGITORY
 
 docker build -t $ECR_REPOGITORY containers/train/
 docker tag "${ECR_REPOGITORY}${IMAGE_TAG}" $IMAGE_URI
+docker tag "${ECR_REPOGITORY}:latest" $IMAGE_URI
 docker push $IMAGE_URI
 
 echo "Container registered. URI:${IMAGE_URI}"
@@ -35,6 +32,7 @@ aws ecr create-repository --repository-name $ECR_REPOGITORY
 
 docker build -t $ECR_REPOGITORY containers/evaluate/
 docker tag "${ECR_REPOGITORY}${IMAGE_TAG}" $IMAGE_URI
+docker tag "${ECR_REPOGITORY}:latest" $IMAGE_URI
 docker push $IMAGE_URI
 
 echo "Container registered. URI:${IMAGE_URI}"
