ecc4532ab104af5e03e987bb460af0341d69bf06
Mari Ohbuchi <ohbuchim@amazon.co.jp>
2021-11-08 09:43:36 +0900

    test

--- setenv.sh
+++ setenv.sh
@@ -1,3 +1,4 @@
 ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
 ECR_REPOGITORY_PREFIX=$(python setenv.py config image-name-prefix)
+echo "aaaaa ${ECR_REPOGITORY_PREFIX}"
 # REGION=$(python setenv.py config region)
