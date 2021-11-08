09a25205d4a0fe6b2a6f917aa2ec31348cee5565
Mari Ohbuchi <ohbuchim@amazon.co.jp>
2021-11-08 12:38:30 +0900

    test

--- setenv.sh
+++ setenv.sh
@@ -1,8 +1,3 @@
-ECR_REPOGITORY_PREFIX=$(python setenv.py config image-name-prefix)
-JOB_NAME=$(python setenv.py config job-name)
 ACCOUNT_ID=$(aws sts get-caller-identity --query 'Account' --output text)
-TRAIN_IMAGE_URL=$(python setenv.py train image-uri)
-python --version
-echo "ddddd ${TRAIN_IMAGE_URL}"
-echo "aaaaa ${ACCOUNT_ID}"
+ECR_REPOGITORY_PREFIX=$(python setenv.py config image-name-prefix)
 # REGION=$(python setenv.py config region)
