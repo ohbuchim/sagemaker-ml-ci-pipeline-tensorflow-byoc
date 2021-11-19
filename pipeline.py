2fc6e308350830d6f7ce415920b4f9840028a8db
EC2 Default User <ec2-user@ip-172-31-11-124.ec2.internal>
2021-11-19 11:04:33 +0000

    test

--- pipeline.py
+++ pipeline.py
@@ -135,8 +135,7 @@ def create_estimator(params, job_name, sagemaker_role):
 
 def create_training_step(params, estimator, execution_input):
     prepro_output_data = params['prep-output-path']
-    training_input = TrainingInput(s3_data=prepro_output_data,
-                                   input_mode='FastFile')
+    training_input = TrainingInput(s3_data=prepro_output_data)
 
     training_step = TrainingStep(
         "SageMaker Training Step",
