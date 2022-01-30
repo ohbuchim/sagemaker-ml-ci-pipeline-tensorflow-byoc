0a19937e6a880d062e9d13d974b3f994f63ba019
elecho1 <rikiauczatu@gmail.com>
2022-01-30 18:43:41 +0900

    added echo debugs.

--- setenv.sh
+++ setenv.sh
@@ -8,3 +8,8 @@ EVAL_REPORT_S3=$(python setenv.py evaluate result-path)/${EVAL_JOB_NAME}/evaluat
 MLFLOW_SERVER_URI=$(python setenv.py experiments mlflow-server-uri)
 # REGION=$eval
 # REGION=$(python setenv.py config region)
+
+echo "1st"
+echo ${EXEC_ID}
+echo "2nd"
+echo $EXEC_ID
